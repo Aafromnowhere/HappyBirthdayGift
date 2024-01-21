510b2adc96d13091ac957b8ae80bd0134b4beb16
KimTaeman <imgonnabeaprogrammer@gmail.com>
2024-01-21 17:43:45 +0630

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,7 +26,7 @@ body.addEventListener('click', function () {
 
 // Clue 1
 $("#startJourney").click(function () {
-    window.location.href = "HappyBirthdayGift/clue1";
+    window.location.href = "/HappyBirthdayGift/clue1";
 });
 
 console.clear();
